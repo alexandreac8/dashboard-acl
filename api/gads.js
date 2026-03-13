231d929de89b306ae0cfca631c0be5730f029e3f
Alexandre Avila <alexandre.avilac@gmail.com>
2026-03-13 19:07:13 -0300

    fix: usa nova MCC (Academia de Libras) como login-customer-id

--- api/gads.js
+++ api/gads.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
         headers: {
           "Authorization":     `Bearer ${tokenData.access_token}`,
           "developer-token":   process.env.GADS_DEVELOPER_TOKEN,
-          "login-customer-id": customerId,
+          "login-customer-id": "6954291013",
           "Content-Type":      "application/json",
         },
         body: JSON.stringify({ query }),
